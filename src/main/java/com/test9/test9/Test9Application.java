dd601c85c96e30b6be4676ba95213dba9839b6e9
shakeel <self.shakeelahmed@gmail.com>
2025-03-13 12:14:56 +0530

    first commit

---
+++ src/main/java/com/test9/test9/Test9Application.java
@@ -0,0 +1,13 @@
+package com.test9.test9;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Test9Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Test9Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/test9/test9/Test9ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test9.test9;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Test9ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
